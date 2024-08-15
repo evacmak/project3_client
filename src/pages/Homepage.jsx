ee2b03714d3729dd8b56a126993b43e637f391bf
“Eva <evacmak@outlook.pt>
2024-08-15 11:58:56 +0100

    few updates

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,19 @@
 import { useContext } from 'react';
 import { AuthContext } from '../context/auth.context';
 import banner from '../assets/images/banner.png';
+import blushSpicy from '../assets/images/blush-spicy.webp';
 
 const Homepage = () => {
   useContext(AuthContext);
   return (
-    <div className='mx-4 bg-[#FAF5F1]'>
+    <div className='mx-4 bg-[##FFFFFF]'>
+      <div>
+        <img
+          className='w-[500px] rounded-lg'
+          src={blushSpicy}
+          alt='image blush'
+        />
+      </div>
       <img
         className='w-full rounded-lg mt-4'
         src={banner}
