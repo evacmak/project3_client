bf1f15b472012d391312d887ecd1ea23cd1a3d6b
“Eva <evacmak@outlook.pt>
2024-08-28 18:00:16 +0100

    login e signup

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -41,33 +41,104 @@ function Login() {
 
   return (
     <div className='LoginPage mt-24'>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor='email'>Email</label>
-        <input
-          type='email'
-          name='email'
-          id='email'
-          value={email}
-          onChange={handleEmail}
+      <div className='w-full h-64 overflow-hidden mt-24 px-4 rounded-lg relative'>
+        <img
+          src='https://www.rhodeskin.com/cdn/shop/files/lip-set-1_1800x.jpg?v=1721356290'
+          alt='Signup Banner'
+          className='w-full h-full object-cover rounded-lg'
         />
-        <label htmlFor='password'>Password</label>
-        <input
-          type='password'
-          name='password'
-          id='password'
-          value={password}
-          onChange={handlePassword}
+        <div className='absolute inset-0 flex items-center justify-center'>
+          <h1
+            className='text-white text-5xl font-medium drop-shadow-xl'
+            style={{ fontFamily: 'kanit, extrabold' }}>
+            Login for exclusive deals.
+          </h1>
+        </div>
+      </div>
+
+      {/* Columns Container */}
+      <div className='flex items-center justify-center mt-10 px-4'>
+        {/* Column 1: Form */}
+        <div className='md:w-1/3 flex flex-col items-center justify-center'>
+          <p
+            className='text-center text-4xl font-medium mb-8'
+            style={{ fontFamily: 'kanit, extrabold' }}>
+            Signup
+          </p>
+
+          <form
+            onSubmit={handleSubmit}
+            className='w-full max-w-sm flex flex-col space-y-4'>
+            <label htmlFor='email'>Email</label>
+            <input
+              type='email'
+              name='email'
+              id='email'
+              value={email}
+              onChange={handleEmail}
+              className='border-2 rounded-full'
+            />
+            <label htmlFor='password'>Password</label>
+            <input
+              type='password'
+              name='password'
+              id='password'
+              value={password}
+              onChange={handlePassword}
+              className='border-2 rounded-full'
+            />
+            <button
+              type='submit'
+              className='mt-4 w-full py-2 rounded-full border-2 hover:bg-[#ddd]'>
+              Login
+            </button>
+          </form>
+
+          {errorMessage && <p>{errorMessage}</p>}
+        </div>
+
+        {/* Column 2: Additional Image (optional) */}
+        <div className='md:w-1/3 flex items-center justify-center'>
+          <img
+            src='https://www.rhodeskin.com/cdn/shop/files/PDP-Desktop-2_49f35727-c353-45dd-879e-88609689320e_1800x.jpg?v=1707782607'
+            alt='Signup illustration'
+            className='w-full max-w-md rounded-lg'
+          />
+        </div>
+
+        {/* Column 3: Login Link */}
+        <div className='md:w-1/3 flex flex-col items-center justify-center'>
+          {errorMessage && <p className='error-message'>{errorMessage}</p>}
+
+          <p
+            className='text-center text-4xl font-medium mb-8'
+            style={{ fontFamily: 'kanit, extrabold' }}>
+            Don't have an account?
+          </p>
+
+          <Link
+            to='/signup'
+            className='mt-4 w-full py-2 rounded-full border-2 hover:bg-[#ddd] text-center flex items-center justify-center'>
+            Sign up
+          </Link>
+        </div>
+      </div>
+
+      <div className='w-full h-64 overflow-hidden mt-10 px-4 rounded-lg relative'>
+        <img
+          src='https://www.rhodeskin.com/cdn/shop/files/Mask_Group-1_97d2ee21-b57b-4ad7-9f35-ad98a4db463f_1800x.jpg?v=1695070880'
+          alt='Signup Banner'
+          className='w-full h-full object-cover rounded-lg'
         />
-        <button type='submit'>Login</button>
-      </form>
-
-      {errorMessage && <p>{errorMessage} </p>}
-
-      <p>Don`t have an account?</p>
-      <Link to='/signup'>Signup</Link>
+        <div className='absolute inset-0 flex items-center justify-center'>
+          <h1
+            className='text-white text-5xl font-medium drop-shadow-xl'
+            style={{ fontFamily: 'kanit, extrabold' }}>
+            Join us on the rhode to an effortless glow.
+          </h1>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default Login;
--- src/pages/Auth/Signup.jsx
+++ src/pages/Auth/Signup.jsx
@@ -18,12 +18,9 @@ function Signup() {
   const handleLastName = (event) => setLastName(event.target.value);
 
   const handleSubmit = async (event) => {
-    event.preventDefault(); //isto faz com que a página não carregue como faz por predefinição
+    event.preventDefault();
 
     try {
-      //Same as the process.env that we saw on the server, but for Vite
-      //Estamos a importar o link que temos no .env file que se chama API_URL e depois adicionar o caminho que queremos
-      //o segundo argumento é o req.body do servidor
       await axios.post(`${import.meta.env.VITE_API_URL}/auth/signup`, {
         name,
         email,
@@ -37,90 +34,127 @@ function Signup() {
   };
 
   return (
-    <div className='SignupPage flex items-center justify-center min-h-screen px-4'>
-      {/* Column 1: Form */}
-      <div className='md:w-1/3 flex flex-col items-center justify-center'>
-        <p
-          className='text-center text-4xl font-medium mb-8'
-          style={{ fontFamily: 'kanit, extrabold' }}>
-          Signup
-        </p>
+    <div className='SignupPage'>
+      {/* Banner Image with Custom Height */}
 
-        <form
-          onSubmit={handleSubmit}
-          className='w-full max-w-sm flex flex-col space-y-4'>
-          <label htmlFor='name'>Name</label>
-          <input
-            type='text'
-            name='name'
-            id='name'
-            value={name}
-            onChange={handleName}
-            className='border-2 rounded-full'
-          />
-
-          <label htmlFor='lastName'>Last Name</label>
-          <input
-            type='text'
-            name='lastName'
-            id='lastName'
-            value={lastName}
-            onChange={handleLastName}
-            className='border-2 rounded-full'
-          />
-
-          <label htmlFor='email'>Email</label>
-          <input
-            type='email'
-            name='email'
-            id='email'
-            value={email}
-            onChange={handleEmail}
-            className='border-2 rounded-full'
-          />
-
-          <label htmlFor='password'>Password</label>
-          <input
-            type='password'
-            name='password'
-            id='password'
-            value={password}
-            onChange={handlePassword}
-            className='border-2 rounded-full'
-          />
-
-          <button
-            disabled={loading}
-            type='submit'
-            className='mt-4 w-full py-2 rounded-full border-2 hover:bg-[#ddd]'>
-            Signup
-          </button>
-        </form>
-      </div>
-
-      {/* Column 2: Image */}
-      <div className='md:w-1/3 flex items-center justify-center'>
+      <div className='w-full h-64 overflow-hidden mt-24 px-4 rounded-lg relative'>
         <img
-          src='https://www.rhodeskin.com/cdn/shop/files/carousel-peach-duo_1800x.jpg?v=1718218851'
-          alt='Signup illustration'
-          className='w-full max-w-md rounded-lg'
+          src='https://www.rhodeskin.com/cdn/shop/files/coral-duo-hero-1_10_1800x.jpg?v=1718244705'
+          alt='Signup Banner'
+          className='w-full h-full object-cover rounded-lg'
         />
+        <div className='absolute inset-0 flex items-center justify-center'>
+          <h1
+            className='text-white text-5xl font-medium drop-shadow-xl'
+            style={{ fontFamily: 'kanit, extrabold' }}>
+            Sign up for exclusive deals.
+          </h1>
+        </div>
       </div>
-      {/* Column 3 */}
-      <div className='md:w-1/3 flex flex-col items-center justify-center'>
-        {errorMessage && <p className='error-message'>{errorMessage} </p>}
 
-        <p
-          className='text-center text-4xl font-medium mb-8'
-          style={{ fontFamily: 'kanit, extrabold' }}>
-          Already have an account?
-        </p>
+      {/* Columns Container */}
+      <div
+        className='flex items-center justify-center mt-10
+       px-4'>
+        {/* Column 1: Form */}
+        <div className='md:w-1/3 flex flex-col items-center justify-center'>
+          <p
+            className='text-center text-4xl font-medium mb-8'
+            style={{ fontFamily: 'kanit, extrabold' }}>
+            Signup
+          </p>
 
-        <Link
-          to='/login'
-          className='mt-4 w-full py-2 rounded-full border-2 hover:bg-[#ddd] text-center flex items-center justify-center'>
-          Login
-        </Link>
+          <form
+            onSubmit={handleSubmit}
+            className='w-full max-w-sm flex flex-col space-y-4'>
+            <label htmlFor='name'>Name</label>
+            <input
+              type='text'
+              name='name'
+              id='name'
+              value={name}
+              onChange={handleName}
+              className='border-2 rounded-full'
+            />
+
+            <label htmlFor='lastName'>Last Name</label>
+            <input
+              type='text'
+              name='lastName'
+              id='lastName'
+              value={lastName}
+              onChange={handleLastName}
+              className='border-2 rounded-full'
+            />
+
+            <label htmlFor='email'>Email</label>
+            <input
+              type='email'
+              name='email'
+              id='email'
+              value={email}
+              onChange={handleEmail}
+              className='border-2 rounded-full'
+            />
+
+            <label htmlFor='password'>Password</label>
+            <input
+              type='password'
+              name='password'
+              id='password'
+              value={password}
+              onChange={handlePassword}
+              className='border-2 rounded-full'
+            />
+
+            <button
+              disabled={loading}
+              type='submit'
+              className='mt-4 w-full py-2 rounded-full border-2 hover:bg-[#ddd]'>
+              Signup
+            </button>
+          </form>
+        </div>
+
+        {/* Column 2: Additional Image (optional) */}
+        <div className='md:w-1/3 flex items-center justify-center'>
+          <img
+            src='https://www.rhodeskin.com/cdn/shop/files/carousel-peach-duo_1800x.jpg?v=1718218851'
+            alt='Signup illustration'
+            className='w-full max-w-md rounded-lg'
+          />
+        </div>
+
+        {/* Column 3: Login Link */}
+        <div className='md:w-1/3 flex flex-col items-center justify-center'>
+          {errorMessage && <p className='error-message'>{errorMessage}</p>}
+
+          <p
+            className='text-center text-4xl font-medium mb-8'
+            style={{ fontFamily: 'kanit, extrabold' }}>
+            Already have an account?
+          </p>
+
+          <Link
+            to='/login'
+            className='mt-4 w-full py-2 rounded-full border-2 hover:bg-[#ddd] text-center flex items-center justify-center'>
+            Login
+          </Link>
+        </div>
+      </div>
+      <div className='w-full h-64 overflow-hidden mt-10 px-4 rounded-lg relative'>
+        <img
+          src='https://www.rhodeskin.com/cdn/shop/files/Mask_Group-1_97d2ee21-b57b-4ad7-9f35-ad98a4db463f_1800x.jpg?v=1695070880'
+          alt='Signup Banner'
+          className='w-full h-full object-cover rounded-lg'
+        />
+        <div className='absolute inset-0 flex items-center justify-center'>
+          <h1
+            className='text-white text-5xl font-medium drop-shadow-xl'
+            style={{ fontFamily: 'kanit, extrabold' }}>
+            Join us on the rhode to an effortless glow.
+          </h1>
+        </div>
       </div>
     </div>
   );
